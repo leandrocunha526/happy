67e8bbf9003d417ef43eccab6c569f44d21f5552
leandrocunha526 <leandrocunha526@gmail.com>
2020-10-15 02:51:39 -0300

    Add pages, components and styles

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -3,15 +3,21 @@ import React from "react";
 import {BrowserRouter, Switch, Route} from "react-router-dom";
 
 import Landing from "./pages/Landing";
+import OrphanagesMap from "./pages/OrphanagesMap";
+import Orphanage from "./pages/OrphanagesMap";
+import CreateOrphanage from "./pages/CreateOrphanage";
 
 const Routes: React.FC = () => {
     return(
     <BrowserRouter>
     <Switch>
     <Route path="/" exact component={Landing}/>
+    <Route path="/app" component={OrphanagesMap}/>
+    <Route path="/orphanages/create" component={CreateOrphanage}/>
+    <Route path="/orphanages/:id" component={Orphanage}/>
     </Switch>
     </BrowserRouter>
-    )
-}
+    );
+};
 
 export default Routes;
