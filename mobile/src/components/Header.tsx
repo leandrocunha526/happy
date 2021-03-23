481444dab5e0063f06562bd39a3d8c7049723493
leandrocunha526 <leandrocunha016@gmail.com>
2021-03-23 15:34:39 -0300

    Import components

---
+++ mobile/src/components/Header.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { BorderlessButton } from "react-native-gesture-handler";
+import { Feather } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+
+interface HeaderProps {
+  showCancel?: boolean;
+  title: string;
+}
+
+const Header: React.FC<HeaderProps> = ({ title, showCancel = true }) => {
+  const { goBack, navigate } = useNavigation();
+  return (
+    <View style={styles.container}>
+      <BorderlessButton onPress={goBack}>
+        <Feather name="arrow-left" size={24} color="#15b5d6"></Feather>
+      </BorderlessButton>
+      <Text style={styles.title}>{title}</Text>
+      {showCancel ? (
+        <BorderlessButton onPress={() => navigate("OrphanageMap")}>
+          <Feather name="x" size={24} color="#ff669d"></Feather>
+        </BorderlessButton>
+      ) : (
+        <View />
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    backgroudColor: "#f9fafc",
+    borderBottonWidth: 1,
+    borderColor: "#dde3f0",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 24,
+    paddingTop: 44,
+  },
+  title: {
+    color: "#8fa7b3",
+    fontFamily: "Nunito_600SemiBold",
+    fontSize: 16,
+  },
+});
+
+export default Header;
